009b3dfc11791262251c1aca26210dbc864c6e6c
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-06-17 01:16:04 -0400

    More practice

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,8 @@ require_relative '../config/environment'
 require 'rack/test'
 require 'database_cleaner'
 
-if ActiveRecord::Migrator.needs_migration?
-  raise 'Migrations are pending. Run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
+if ActiveRecord::Base.connection.migration_context.needs_migration?
+  raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
 RSpec.configure do |config|
